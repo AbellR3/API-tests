8476d95aba804c7cd56f56442b602403c76ebdfb
Roman Mihaylov <>
2022-05-19 11:27:50 +0400

    m

--- tests/auth/get/test_get_profiles.py
+++ tests/auth/get/test_get_profiles.py
@@ -1,10 +1,8 @@
-
-from urllib.request import HTTPCookieProcessor
 from tests.src.pydandic_shemas.get_prifiles_schema import Profile
 import requests
 from base import AuthorisedHeader, ResponseValidator
 from STATIC import auth_url
-import random
+
 
 
 def test_get_profiles(user_data):
